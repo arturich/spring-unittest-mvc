054f6c65a78da78718c92619ab718393dd382a74
arturich <arturich@gmail.com>
2022-04-27 14:56:58 -0500

    Adding more test that uses services and creating all the logic through the DAO

--- src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -1,5 +1,7 @@
 package com.luv2code.springmvc.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,27 @@ public class StudentAndGradeService {
 		student.setId(0);
 		
 		studentDao.save(student);
+	}
+	
+	public boolean checkIfStudentIsNull(int id)
+	{
+		Optional<CollegeStudent> student = studentDao.findById(id);
+		
+		if(student.isPresent())
+			return true;
+		
+		return false;
+	}
+
+	public void deleteStudent(int id) {
+		
+		studentDao.deleteById(id);
+		
+	}
+
+	public void deleteStudent(CollegeStudent student) {
+		studentDao.delete(student);
+		
 	}
 
 }
--- src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,10 +1,18 @@
 package com.luv2code.springmvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
 import com.luv2code.springmvc.models.CollegeStudent;
@@ -20,8 +28,19 @@ class StudentAndGradeServiceTest {
 	
 	@Autowired
 	private StudentDao studentDao;
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@BeforeEach
+	public void setupDatabase()
+	{
+		jdbcTemplate.execute("INSERT INTO student(id,firstname,lastname,email_address)"
+				+ " values(1,'Eric','Roby','eric_roby@luv2code_school.com')");
+	}
 
 	@Test
+	//@Disabled("Dont use it temporally")
 	void createStudentService() {
 		
 		studentService.createStudent("Chad","Darby","chad.darby@luv2code_school.com");
@@ -31,5 +50,47 @@ class StudentAndGradeServiceTest {
 		assertEquals("chad.darby@luv2code_school.com",student.getEmailAddress(),"find by email");
 		
 	}
+	
+	
+	@Test
+	@DisplayName("Check if student is null")
+	public void isStudentNullCheck()
+	{
+		assertTrue(studentService.checkIfStudentIsNull(1));
+		assertFalse(studentService.checkIfStudentIsNull(0));
+		
+	}
+	
+	@Test
+	@DisplayName("Delete the student by ID using service")
+	public void deleteStudentById()
+	{
+		int id = 1;
+		studentService.deleteStudent(id);
+		
+		assertFalse(studentService.checkIfStudentIsNull(id));
+	}
+	
+	@Test
+	@DisplayName("Delete the student by object using service")
+	public void deleteStudentByObject()
+	{
+		int id = 1;
+		Optional<CollegeStudent> student = studentDao.findById(id);
+		if(student.isPresent())
+		{
+			studentService.deleteStudent(student.get());
+		}
+		
+		assertFalse(studentService.checkIfStudentIsNull(id));
+	}
+	
+	
+	@AfterEach
+	public void setupAfterTransaction()
+	{
+		jdbcTemplate.execute("DELETE FROM student");
+	}
+	
 
 }
